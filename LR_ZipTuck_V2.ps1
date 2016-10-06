70f963ae84595434f8d51d983ebf835de1bc0a18
Richard Hewlett <rich_hewlett@yahoo.com>
2016-10-06 21:15:57 +0100

    Remote copy line fixed

--- LR_ZipTuck_V2.ps1
+++ LR_ZipTuck_V2.ps1
@@ -1,12 +1,10 @@
 ﻿#==========================================================================================================
-# Renames Adobe Lightroom catalogue backups with date, flattens folder structure, copy to remote location, 
-# retaining some zip files locally. Also housekeeps the server location (maintains X number of zip files). 
-# Just run this after exiting Lightroom (or anytime you want to clean up LR backups). If LR is running it 
-# will wait for it to close. 
-# This is for Lightroom V6 upwards (for older versions use the V1 version of this script)
+# Zip up Lightroom catalogue backup folders, copy to Server and delete, retaining some zip files locally
+# Also housekeeps the server location (maintains X number of zip files) 
+# Author: RichHewlett.com
 # ---------------------------------------------------------------------------------------------------------
-# 02/01/14 : V1 Creation
-# 25/09/16 : V2 Creation. Removed Zip functionality as Lightroom v6 now automatcially compresses backups 
+# 02/01/14 : Creation
+# 25/09/16 : V2 version removes Zip functionality as LR v6 no auto zips backups 
 #==========================================================================================================
 
 # clear screen
@@ -17,10 +15,9 @@ write-output "------------------------------------Script Start------------------
 write-output "Lightroom Backup Zip Tuck V2" 
 write-output "------------------------------------------------------------------------------------" 
 
-# load modules used in this script, change path as required
+# load modules used in this script
 import-module -name C:\scripts\support\SupportModule -verbose
 
-# local folder where LR dumps your backups, and a folder on the network or another drive to duplicate to 
 $LocalBkUpFolder = "D:\LightroomBackUp"
 $RemoteBkUpFolder = "X:\LightroomBackup\CatalogBkUps"
 
@@ -34,7 +31,7 @@ if($process)
     start-sleep -s 2
 }
 
-Write-Output "Sorting backup file(s) for processing..."
+Write-Output "Sorting backup file(s)..."
 
 ## look for folders (unzipped backups) in backup location
 if ((Get-ChildItem -Path $LocalBkUpFolder -Directory).Count -gt 0)
@@ -49,8 +46,9 @@ if ((Get-ChildItem -Path $LocalBkUpFolder -Directory).Count -gt 0)
         $SourceFilePath = $path.FullName + "\" + $path.Name + ".zip"       
         Move-Item $SourceFilePath -Destination $LocalBkUpFolder 
 
-        ## copy zip to remote share location 
+        ## copy zip to P:\ server location 
         Write-Output "Tucking backup away on remote share"
+        $NewFileName = $LocalBkUpFolder + "\" + $path.Name + ".zip"
         Copy-Item $NewFileName -Destination $RemoteBkUpFolder
         
         ## delete folder
@@ -58,12 +56,12 @@ if ((Get-ChildItem -Path $LocalBkUpFolder -Directory).Count -gt 0)
     }
 }
 
-## cleanup zip files (local)
+## cleanup zip files
 write-output "Clear out old zipped files (local)..." 
 Remove-MostFiles $LocalBkUpFolder *.zip 8
 
-## cleanup zip files (remote)
-write-output "Clear out old zipped files (remote)..." 
+## cleanup zip files
+write-output "Clear out old zipped files (Server)..." 
 Remove-MostFiles $RemoteBkUpFolder *.zip 20
 
 Write-Output "Script Complete !!!"
